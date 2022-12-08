1f10725527985264c297b95d5183d931988abccf
“panjingsheng” <1766048875@qq.com>
2022-12-08 14:01:29 +0800

    提交dev

--- src/main/java/com/bjpowernode/service/impl/serviceimpl02.java
+++ src/main/java/com/bjpowernode/service/impl/serviceimpl02.java
@@ -2,9 +2,12 @@ package com.bjpowernode.service.impl;
 
 import com.bjpowernode.service.testservice;
 
-public class serviceimpl implements testservice {
+public class serviceimpl02 implements testservice {
     @Override
     public void sout() {
         System.out.println("222222");
+    }
+    public void soutdev() {
+        System.out.println("dev");
     }
 }
