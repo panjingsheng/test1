ec0a2372630ea7a3218496c664fd0f5931b8a0d0
“panjingsheng” <1766048875@qq.com>
2022-12-08 14:25:57 +0800

    dev tag

--- src/main/java/com/bjpowernode/controller/test01.java
+++ src/main/java/com/bjpowernode/controller/test01.java
@@ -1,9 +1,17 @@
 package com.bjpowernode.controller;
 
+import com.bjpowernode.service.testservice;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class test01 {
 
+@Autowired
+    private testservice testservice01;
+
+    public  void test() {
+        testservice01.sout();
+    }
 
 }
--- src/main/java/com/example/demo01/Demo01Application.java
+++ src/main/java/com/example/demo01/Demo01Application.java
@@ -1,13 +1,20 @@
 package com.example.demo01;
 
+import com.bjpowernode.controller.test01;
+import com.bjpowernode.service.testservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class Demo01Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo01Application.class, args);
+
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Demo01Application.class, args);
+        testservice testservice = (com.bjpowernode.service.testservice) configurableApplicationContext.getBean("testservice");
+        testservice.sout();
     }
 
 }
